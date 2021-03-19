c7397f610992269ebbaca3b2ce97cdd77c93b775
Dean-22 <79496319+Dean-22@users.noreply.github.com>
2021-03-19 19:16:58 +0800

    Update Study1.java

--- Study1.java
+++ Study1.java
@@ -1,2 +1,13 @@
-pulic class Study{ }
- @methods
+package com.calculator;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+
+@WebService
+public class Calculator {
+    @WebMethod
+    
+    public String Name(String name){
+        return name;
+    }
+}
